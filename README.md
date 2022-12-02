dc4a7c3af5ffaf2024e2c094eb234bc3708e63dd
Emmanuella-codes <81877624+Emmanuella-codes@users.noreply.github.com>
2022-12-02 23:50:33 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# codewars-8kyu-Welcome-
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-w4j7zd)
---
+++ index.js
@@ -0,0 +1,32 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+function greet(language) {
+	let differentLanguages;
+  differentLanguages = {
+    czech: 'Vitejte',
+    danish: 'Velkomst',
+    dutch: 'Welkom',
+    estonian: 'Tere tulemast',
+    finnish: 'Tervetuloa',
+    flemish: 'Welgekomen',
+    french: 'Bienvenue',
+    german: 'Willkommen',
+    irish: 'Failte',
+    italian: 'Benvenuto',
+    latvian: 'Gaidits',
+    lithuanian: 'Laukiamas',
+    polish: 'Witamy',
+    spanish: 'Bienvenido',
+    swedish: 'Valkommen',
+    welsh: 'Croeso',
+  }
+
+  return (differentLanguages[language] || `Welcome`);
+}
+
+console.log(greet('czech'));
