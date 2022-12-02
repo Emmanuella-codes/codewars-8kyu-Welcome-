3b70149c587e98c8905fc6c8c98638441fc3e7a7
Emmanuella-codes <81877624+Emmanuella-codes@users.noreply.github.com>
2022-12-02 23:51:40 +0100

    1 file modified

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
-// Import stylesheets
-import './style.css';
-
 // Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
-
 function greet(language) {
-	let differentLanguages;
+  let differentLanguages;
   differentLanguages = {
     czech: 'Vitejte',
     danish: 'Velkomst',
@@ -24,9 +18,9 @@ function greet(language) {
     spanish: 'Bienvenido',
     swedish: 'Valkommen',
     welsh: 'Croeso',
-  }
+  };
 
-  return (differentLanguages[language] || `Welcome`);
+  return differentLanguages[language] || `Welcome`;
 }
 
 console.log(greet('czech'));
